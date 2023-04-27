f23cca2e1785d100982877e5be21c1e52c59dc9f
sargsyansevak <thesevaksargsyan@gmail.com>
2023-04-27 21:00:22 +0400

    header fixing

--- components/PageImage/PageImage.tsx
+++ components/PageImage/PageImage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { motion as m } from "framer-motion";
-
+import { useTransform } from "framer-motion";
+import { useMotionValue } from "framer-motion";
 export const PageImage = ({ url }: any) => {
   const [position, setPosition] = useState<{ x: number; y: number }>({
     x: 0,
@@ -21,7 +22,24 @@ export const PageImage = ({ url }: any) => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
+  const [hasScrolled, setHasScrolled] = useState(false);
+  const scrollY = useMotionValue(0);
+  const y = useTransform(scrollY, [0, 100], ["0%", "-100%"]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      scrollY.set(window.scrollY);
+      if (window.scrollY > 100) {
+        setHasScrolled(true);
+      } else {
+        setHasScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollY]);
   return (
     <m.div
       animate={{ x: 0, opacity: 1 }}
@@ -31,6 +49,11 @@ export const PageImage = ({ url }: any) => {
         duration: 0.5,
         type: "spring",
         stiffness: 120,
+      }}
+      style={{
+        y: y,
+        opacity: hasScrolled ? 0 : 1,
+        transition: "all 0.8s easeIn",
       }}
     >
       <img
