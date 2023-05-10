d95ab36f57da063400583848e1604daa4d9b1e98
sargsyansevak <thesevaksargsyan@gmail.com>
2023-05-11 00:05:21 +0400

    done

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -3,74 +3,27 @@ import { motion as m } from "framer-motion";
 import { PageImage } from "../PageImage/PageImage";
 import Head from "next/head";
 const Content = () => {
-  
   return (
-    <section>
+    <section className="page">
       <Head>
         <title>Home</title>
         <meta name="description" content="sargsyan" />
       </Head>
-      <m.div className="bg-yellow-400 absolute top-0 left-0  w-full h-screen flex justify-center items-center"
-       animate={{ x: "0%" }}
-       exit={{ opacity: 1 }}
-       initial={{ x: "100%" }}
-       transition={{ duration: 0.8, ease: "easeOut" }}
+      <m.div
+        className="bg-yellow-400 absolute top-0 left-0  w-full h-screen flex justify-center items-center"
+        animate={{ x: "0%" }}
+        exit={{ opacity: 1 }}
+        initial={{ x: "100%" }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
       >
-        {/* <h5 className="text-white font-bold text-9xl">Home</h5> */}
-        <PageImage url='https://clouddevs.com/3dbay/files/preview/960x1152/11632331823h6pir0uj7m71aadwkcup7efglgbrnyesqchv7gu2weovjf62e3lurbqziakkof9yhjmonbtjlxcrjlye3oqcaav0w3e9moxusqdz.png'/>
+        <PageImage url="https://clouddevs.com/3dbay/files/preview/960x1152/11632331823h6pir0uj7m71aadwkcup7efglgbrnyesqchv7gu2weovjf62e3lurbqziakkof9yhjmonbtjlxcrjlye3oqcaav0w3e9moxusqdz.png" />
       </m.div>
-      
-      <div className="bg-green-500">
-        <div className="container ">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga laborum
-          ex eligendi, harum rerum eos impedit, nisi, vel exercitationem dolorem
-          veritatis libero. Eius odio eligendi error cum quod obcaecati
-          accusamus assumenda voluptates est vel. Fuga similique dicta neque
-          laborum aperiam consequuntur, officiis, molestiae consequatur unde,
-          assumenda eius sunt qui tenetur. Nisi veritatis nemo, aliquid
-          recusandae cumque quis exercitationem molestiae delectus. Culpa error
-          expedita consequuntur aspernatur placeat iste, nihil tempora
-          doloremque sed repudiandae obcaecati consequatur, itaque numquam!
-          Odit, quas incidunt a dolorum saepe autem neque sed ipsa eos aliquam
-          eveniet alias quia deserunt ducimus asperiores atque dolore laborum
-          vel cupiditate consequuntur quam veritatis commodi quaerat. Dicta
-          corporis hic nesciunt, labore error excepturi perspiciatis iste
-          cumque. Vero autem quam quo ex. Sapiente tenetur quaerat nisi vitae
-          pariatur? Modi id blanditiis officiis cumque totam dolorem saepe
-          quisquam voluptate aliquam odio. Quas eligendi unde nulla. Velit
-          dolorum ullam, doloremque laborum tempore dolor repellendus deserunt,
-          voluptatem impedit distinctio ad, iure reprehenderit odit aspernatur!
-          Beatae, minima aliquam? Veniam porro molestias recusandae fugiat minus
-          temporibus expedita. Vel quis numquam cumque et explicabo fugit,
-          provident deserunt repellat vitae, nulla, ipsum quisquam ea magnam
-          recusandae aliquid architecto. Reiciendis ab aliquam expedita veniam
-          iure nihil pariatur. Obcaecati, beatae? Harum sint optio laudantium
-          debitis alias consequuntur quos, quibusdam, nihil placeat doloribus
-          ullam similique, ut hic sequi nobis odio eius? Repudiandae aperiam
-          natus molestiae distinctio, repellat architecto quam beatae libero
-          vero necessitatibus. Nihil, eaque provident vero recusandae quod
-          sapiente est, deleniti eligendi ullam eum, doloribus incidunt
-          voluptate temporibus possimus maxime iure aperiam pariatur sint cum
-          aspernatur adipisci a tempora! Veniam ratione id quo corporis rerum
-          aliquam at facere ducimus? Pariatur possimus vero quas libero,
-          repudiandae, magni ratione, accusamus optio voluptatibus vel quasi
-          delectus totam. Enim consequatur voluptates optio rerum. Itaque,
-          dolore doloribus necessitatibus fuga nihil quam quis perferendis
-          perspiciatis impedit quae! Assumenda cupiditate nisi tenetur
-          repellendus consequatur magni, omnis earum consequuntur sunt corporis
-          maiores deserunt, illo porro harum molestiae excepturi voluptatem.
-          Similique enim suscipit itaque alias? Quae pariatur natus corrupti
-          quam unde modi dolorum magni quibusdam nobis praesentium, assumenda
-          ducimus doloribus, cum repellat dolore quos facere ex harum aspernatur
-          error officiis aut tempora explicabo perferendis. Pariatur veniam rem
-          dolorem est itaque eaque voluptatibus laboriosam at possimus
-          repudiandae recusandae eos error ad labore, a id sunt doloremque
-          aperiam corporis provident illo harum aliquam dolore! Aliquid suscipit
-          excepturi cupiditate qui voluptatum harum itaque, sint consequuntur
-          error eveniet perferendis eligendi necessitatibus veniam, fuga commodi
-          libero!
-        </div>
-      </div>
+
+      <m.div
+        className="bg-green-500"
+        initial={{ opacity: 0.5 }}
+        whileInView={{ opacity: 1 }}
+      ></m.div>
     </section>
   );
 };
